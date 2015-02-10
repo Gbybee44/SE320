a4702850f6ae0e886a1ee2b37a751b6dd5ff6516
Garrison Bybee <bybeeg@PRUXAP03.ERAU.EDU>
2015-02-09 22:30:32 -0700

    Adding Test file:x

---
+++ Test/test_1.adb
@@ -0,0 +1,16 @@
+with Ada.text_io, Ada.Integer_text_io;
+use Ada.text_io, Ada.Integer_text_io;
+
+procedure test_1 is
+
+sum, temp : Integer;
+
+begin
+
+sum := 1;
+temp := (8 mod 3);
+
+Put("The number is");
+put(temp);
+
+end test_1;
