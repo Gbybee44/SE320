bd715a52ec309a67d7c10685c442b1dca3b0361c
Garrison Bybee <bybeeg@PRUXAP03.ERAU.EDU>
2015-02-09 22:44:44 -0700

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ hw1/hw_1.adb
@@ -0,0 +1,13 @@
+
+-- Garrison Bybee
+-- 1/9/15
+-- SE 320 Ada
+-- Hw 1 
+-- The program is a simple Hello world program for Ada
+
+with ada.text_io; use ada.text_io;
+procedure hw_1 is 
+begin 
+  put("Hello from the world of Ada");
+end hw_1;
+
